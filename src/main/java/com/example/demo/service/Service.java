9a5906daf7d067852cd9eca80ad69a6c074ab016
guna144 <gunaapps14@gmail.com>
2019-08-14 08:32:48 +0400

    uploaded demo-app

---
+++ src/main/java/com/example/demo/service/Service.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Service {
+
+	@GetMapping("/test")
+	private String test() {
+		return "hello world!";
+	}
+}
